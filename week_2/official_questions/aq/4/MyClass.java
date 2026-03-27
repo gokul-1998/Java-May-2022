9e3fcd23ec873887fa579932c21586f164df58c2
gokul-1998 <gokulakrishnanm1998@gmail.com>
2026-03-27 14:45:09 +0530

    das

---
+++ week_2/official_questions/aq/4/MyClass.java
@@ -0,0 +1,10 @@
+public class MyClass
+{
+           public static void main(String args[])
+           {
+                  String name1="IITM";
+                  String name2="Java";
+                  name1.concat(name2);
+                  System.out.println(name1);
+           }
+}
